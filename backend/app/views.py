2aade3c03edbe0a8256ac55cf53f066751a329fe
Rsl1122 <djrapitops@hotmail.com>
2019-11-11 16:39:54 +0200

    Defined 3 new routes

--- backend/app/views.py
+++ backend/app/views.py
@@ -4,3 +4,23 @@ from app import app
 @app.route('/')
 def index():
     return '{"json": "true"}'
+
+
+@app.route('/api/<user>/files')
+def list_files(user):
+    # TODO Get files of the user
+    return '{"files:" [{"path": "folder/example.png",'+
+    '"sha1_hash": "43A025512880D4A84012721C4DD82B736988C07D"}]}'
+
+
+@app.route('/api/<user>/files/<path:file_path>')
+def download_file(user, file_path):
+    # TODO Set UPLOAD_FOLDER and add user specific stuff
+    uploads = os.path.join(app.root_path, app.config['UPLOAD_FOLDER'])
+    return send_from_directory(directory=uploads, filename=filename)
+
+
+@app.route('/api/<user>/files' methods=["POST"])
+def upload_file(user):
+    # TODO get the file from the post request
+    return '{"success": "true"}'
