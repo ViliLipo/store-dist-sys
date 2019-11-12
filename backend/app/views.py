584ed61795a3be2cace99c77f7b083a6d416dfef
Vili Lipo <vili96lipo@gmail.com>
2019-11-12 14:03:26 +0200

    file sharing mvp

--- backend/app/views.py
+++ backend/app/views.py
@@ -1,6 +1,6 @@
 from app import app
 from flask import jsonify, send_from_directory
-from app.models import StoredFile
+from app.models import StoredFile, FileShare, Account
 import os
 
 
@@ -27,3 +27,12 @@ def download_file(user, file_path):
 def upload_file(user):
     # TODO get the file from the post request
     return jsonify({"success": True})
+
+
+@app.route("/api/<user>/shared")
+def list_shared_with_user(user):
+    files = FileShare.query.filter(
+        FileShare.userId == user).join(FileShare.fileItem).all()
+    fileDicts = list(map(lambda f: f.fileItem.toDict(), files))
+    print(fileDicts)
+    return jsonify(fileDicts)
